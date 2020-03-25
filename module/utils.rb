6a036e90b12d45d6116d8747752a21336b02db76
Anton Mishin <coal.msk@gmail.com>
2020-03-25 13:07:04 +0300

    Black Jack ver 1

--- module/utils.rb
+++ module/utils.rb
@@ -1,2 +1,46 @@
+require_relative '../model/deck'
+
 module Utils
+
+  def init_stack
+    @card_stack = {}
+    add_card_to_stack
+    add_card_to_stack
+  end
+
+  def add_card_to_stack
+    @cards ||= Deck.new.deck
+    one_card_get(@cards).each { |key, value| @card_stack[key] = value }
+  end
+
+  def one_card_get(deck_hash)
+    @card = {}
+    deck_hash.sort { rand <=> rand }.each do |key, value|
+      @card[key] = value
+      deck_hash.delete(key)
+
+      break if @card.size == 1
+    end
+
+    @card
+  end
+
+  def score
+    @sum = 0
+    @card_stack.each_value { |value| @sum = @sum + value }
+
+    @sum
+  end
+
+  def bet(bet)
+    @bank = @bank - bet
+  end
+
+  def minus_money(money)
+    @bank = @bank - money
+  end
+
+  def plus_money(money)
+    @bank = @bank + money
+  end
 end
